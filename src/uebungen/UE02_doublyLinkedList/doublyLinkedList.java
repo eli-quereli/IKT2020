cbef6576fc1ae51b3cfad21ff20e6f0b7b807d40
Elisabeth Steffen <elisabeth.steffen@student.htw-berlin.de>
2020-08-12 16:08:31 +0200

    update append for dLL

--- src/uebungen/UE02_doublyLinkedList/doublyLinkedList.java
+++ src/uebungen/UE02_doublyLinkedList/doublyLinkedList.java
@@ -13,61 +13,58 @@ g) size - gibt aus, wie viele Elemente in der Liste enthalten sind
  */
 
 
+// divide and conquer bei get() implementieren
+// Element last implementieren?
 
 public class doublyLinkedList {
 
-        private Element first;
-        private Element last;
-        private int size; //number of elements in the list; is increased if an element is appended, prepended, or inserted; and decreased if an element is removed
+    private Element first;
+    // private Element last;
+    private int size; //number of elements in the list; is increased if an element is appended, prepended, or inserted; and decreased if an element is removed
 
 
-        public doublyLinkedList() {
-            this.first = null;
-            this.last = null;
-            this.size = 0;
+    public doublyLinkedList() {
+        this.first = null;
+        // this.last = null;
+        this.size = 0;
+    }
+
+
+    // a) prepend - Element am Anfang einfügen
+    public void prepend(Element e) {
+
+        if (this.isEmpty()) {
+            this.first = e;
+            e.successor = null;
+
+        } else {
+            e.successor = this.first;
+            this.first = e;
         }
 
+        this.size++;
+    }
 
-        // a) prepend - Element am Anfang einfügen
-        public void prepend(Element e) {
 
-            if (this.isEmpty()) {
-                this.first = e;
-                e.successor = null;
+    // b) append - Element am Ende einfügen
+    public void append(Element e) {
 
-            } else {
-                e.successor = this.first;
-                this.first = e;
-            }
+        if (this.isEmpty()) {
 
-            this.size++;
+            this.first = e;
+            e.successor = null;
+
+        } else {
+
+            Element last = this.get(size - 1);
+            last.successor = e;
+            e.predecessor = last;
+
         }
 
+        this.size++;
 
-        // b) append - Element am Ende einfügen
-        public void append(Element e) {
-
-            if (this.isEmpty()) {
-
-                this.first = e;
-                e.successor = null;
-
-            } else {
-
-                Element current = this.first;
-
-                while (current.getSuccessor() != null) {
-
-                    current = current.getSuccessor();
-                }
-
-                e.predecessor = current;
-                current.successor = e;
-                e.successor = null;
-            }
-
-            this.size++;
-        }
+    }
 
 
     // c) insert - Element an einer angegebenen Position einzufügen
@@ -91,124 +88,124 @@ public class doublyLinkedList {
     }
 
 
-        // d) get - ein Element an einer gegebenen Position abrufen
-        public Element get(int index) throws NullPointerException {
+    // d) get - ein Element an einer gegebenen Position abrufen
+    public Element get(int index) throws NullPointerException {
 
-            Element e = this.first;
-            int position = 0;
+        Element e = this.first;
+        int position = 0;
 
-            if (this.isEmpty()) {
-                throw new NullPointerException();
-            } else if (index < 0 || index > this.getSize()) {
-                throw new IndexOutOfBoundsException();
-            } else {
-                while (position < index && e.hasNext()) {
-                    e = e.getSuccessor();
-                    position++;
-                }
-                return e;
+        if (this.isEmpty()) {
+            throw new NullPointerException();
+        } else if (index < 0 || index > this.getSize()) {
+            throw new IndexOutOfBoundsException();
+        } else {
+            while (position < index && e.hasNext()) {
+                e = e.getSuccessor();
+                position++;
             }
-
+            return e;
         }
 
+    }
 
-        // e) remove - Element an einer angegebenen Position löschen
-        public void remove(int index) throws NullPointerException {
 
-            if (this.isEmpty()) {
-                throw new NullPointerException();
-            } else {
-                Element e = this.get(index);
-                Element temp1 = this.get(index - 1);
-                Element temp2 = this.get(index);
+    // e) remove - Element an einer angegebenen Position löschen
+    public void remove(int index) throws NullPointerException {
 
-                temp1.successor = e.getSuccessor();
-                temp2.predecessor = e.getPredecessor();
-            }
+        if (this.isEmpty()) {
+            throw new NullPointerException();
+        } else {
+            Element e = this.get(index);
+            Element temp1 = this.get(index - 1);
+            Element temp2 = this.get(index);
 
-            this.size--;
-
-            if (this.size == 0) {
-                this.first = null;
-                this.first.successor = null;
-            }
+            temp1.successor = e.getSuccessor();
+            temp2.predecessor = e.getPredecessor();
         }
 
-        // f) contains - prüft, ob das übergebene Element in der Liste enthalten ist
-        public boolean contains(Element e) {
+        this.size--;
 
-            if (this.isEmpty()) {
-                return false;
-            } else {
-
-                Element current = this.first;
-
-                while (current.hasNext()) {
-
-                    if (current.equals(e)) {
-                        return true;
-                    } else {
-                        current = current.successor;
-                    }
-                }
-
-                return false;
-            }
+        if (this.size == 0) {
+            this.first = null;
+            this.first.successor = null;
         }
+    }
 
+    // f) contains - prüft, ob das übergebene Element in der Liste enthalten ist
+    public boolean contains(Element e) {
 
-        //returns true if list is empty, false else
-        public boolean isEmpty() {
-            return this.first == null;
-        }
+        if (this.isEmpty()) {
+            return false;
+        } else {
 
-        //returns the number of elements in the list
-        public int getSize() throws NullPointerException {
+            Element current = this.first;
 
-            if (this.isEmpty()) {
+            while (current.hasNext()) {
 
-                throw new NullPointerException();
-            } else {
-                return this.size;
-            }
-        }
-
-
-        //prints all elements of the list from start to end
-        public void print() {
-
-            if (this.isEmpty()) {
-
-                System.out.println("Empty list!");
-
-            } else {
-
-                int i;
-
-                for (i = 0; i < this.getSize(); i++) {
-                    System.out.println(this.get(i).getValue());
+                if (current.equals(e)) {
+                    return true;
+                } else {
+                    current = current.successor;
                 }
             }
+
+            return false;
         }
+    }
 
-        //prints all elements in the list in reverse order (from end to start)
-        public void printReverse() throws NullPointerException {
 
-            if (this.isEmpty()) {
+    //returns true if list is empty, false else
+    public boolean isEmpty() {
+        return this.first == null;
+    }
 
-                System.out.println("Empty list!");
+    //returns the number of elements in the list
+    public int getSize() throws NullPointerException {
 
-            } else {
+        if (this.isEmpty()) {
 
-                int i;
+            throw new NullPointerException();
+        } else {
+            return this.size;
+        }
+    }
 
-                for (i = this.getSize() - 1; i >= 0; i--) {
 
-                    System.out.println(this.get(i).getValue());
-                }
+    //prints all elements of the list from start to end
+    public void print() {
+
+        if (this.isEmpty()) {
+
+            System.out.println("Empty list!");
+
+        } else {
+
+            int i;
+
+            for (i = 0; i < this.getSize(); i++) {
+                System.out.println(this.get(i).getValue());
             }
         }
     }
+
+    //prints all elements in the list in reverse order (from end to start)
+    public void printReverse() throws NullPointerException {
+
+        if (this.isEmpty()) {
+
+            System.out.println("Empty list!");
+
+        } else {
+
+            int i;
+
+            for (i = this.getSize() - 1; i >= 0; i--) {
+
+                System.out.println(this.get(i).getValue());
+            }
+        }
+    }
+}
 
 
 
